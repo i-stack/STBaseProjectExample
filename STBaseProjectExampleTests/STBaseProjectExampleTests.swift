9ba9fce9f73d031bfb6a677043959ad550a00b82
stack <songshoubing7664@163.com>
2026-04-28 17:32:31 +0800

    Refactor Podfile structure and remove unused files. Added targets for testing and removed AppDelegate, SceneDelegate, Info.plist, and various view controllers related to button and HUD testing.

---
+++ STBaseProjectExampleTests/STBaseProjectExampleTests.swift
@@ -0,0 +1,19 @@
+//
+//  STBaseProjectExampleTests.swift
+//  STBaseProjectExampleTests
+//
+//  Created by 寒江孤影 on 2026/4/28.
+//
+
+import Testing
+@testable import STBaseProjectExample
+
+struct STBaseProjectExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
